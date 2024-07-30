5cc9a137a2a70e4a1154ab3094604e1e40a05e09
Theodoros Stefanidis <stefanidhes@gmail.com>
2024-07-30 20:43:26 +0300

    Update Constants.js

--- js/Constants.js
+++ js/Constants.js
@@ -1,4 +1,4 @@
-LANDS = ['144958','144959','144960','144961','144962','144963','144702','144703','144704','144705','144706','144446','144447','144449','144450','144191','144194'];
+LANDS = ['144958', '144959', '144960', '144961', '144962', '144963', '144702', '144703', '144704', '144705', '144706', '144446', '144447', '144449', '144450', '144191', '144194'];
 
 CONTINENT = [65];
 
@@ -6,10 +6,105 @@ PERCENTAGE = parseFloat(0.65);
 
 WALLETS = [
     {
-        "CONTINENT": "65",
-        "kingdomId": "",
-        "wallet": "",
-        "name": ""
+        "CONTINENT": 65,
+        "kingdomId": "6166329e0b0a756f724163db",
+        "wallet": "0x0aa480d09a2301364d6acefaea1d5ca57028e933",
+        "name": "◐ ClanRico ◐"
+    },
+    {
+        "CONTINENT": 65,
+        "kingdomId": "6214c6e045334248c52b2e41",
+        "wallet": "0xe91b53d999b08dbdf40e170206ab840cb1c9cc11",
+        "name": "Beniwal ◈"
+    },
+    {
+        "CONTINENT": 65,
+        "kingdomId": "6226653ae1fda3110747908e",
+        "wallet": "0xC339955CEd00F9400Ba58874EbCb90Eb26b85057",
+        "name": "ESPARTAKO◐"
+    },
+    {
+        "CONTINENT": 65,
+        "kingdomId": "62dd606183e96f6e56ffa9f8",
+        "wallet": "0xCd36e797Ead463e21A852443A04A007Aa496c9cC",
+        "name": "☢️Cali 83 ▶️"
+    },
+    {
+        "CONTINENT": 65,
+        "kingdomId": "631ee4394429d0458a9c4deb",
+        "wallet": "0xCd36e797Ead463e21A852443A04A007Aa496c9cC",
+        "name": "☢️ Cali 83"
+    },
+    {
+        "CONTINENT": 65,
+        "kingdomId": "64f55f0af451b24a74f46b03",
+        "wallet": "0x0DFd857A95d89af429de9f81b61Eaa6Dbe33af87",
+        "name": "Zoodin ◈"
+    },
+    {
+        "CONTINENT": 65,
+        "kingdomId": "65c61fdbfe4358280c431cc0",
+        "wallet": "0x8bef439e63a02e68b37771134d3ffc2649ee8f10",
+        "name": "AryanGreat ◉"
+    },
+    {
+        "CONTINENT": 65,
+        "kingdomId": "660ca4677938b53a121dae00",
+        "wallet": "0xdD244535768A40E3D1Dc4275903A227867399FdE",
+        "name": "MALANDRO▱"
+    },
+    {
+        "CONTINENT": 65,
+        "kingdomId": "66214170c435fa497b481ab7",
+        "wallet": "0xCd36e797Ead463e21A852443A04A007Aa496c9cC",
+        "name": "➡️Cali408™️⬅️"
+    },
+    {
+        "CONTINENT": 65,
+        "kingdomId": "662cf98a030d58963ab53cdf",
+        "wallet": "0x0003fb6b463112b63580eb2c80bdfb4ab452149a",
+        "name": "Sparta $ ◈"
+    },
+    {
+        "CONTINENT": 65,
+        "kingdomId": "62af48e44bb1f3091e24f310",
+        "wallet": "0xe6De8485fAc1Eb4779978Bc455ef09e0E8c107Bb",
+        "name": "JIMBE"
+    },
+    {
+        "CONTINENT": 65,
+        "kingdomId": "63097b4d019ed96d8284ce91",
+        "wallet": "0xe6De8485fAc1Eb4779978Bc455ef09e0E8c107Bb",
+        "name": ".Vinsmoke Sanji"
+    },
+    {
+        "CONTINENT": 65,
+        "kingdomId": "6335412c9379e06ef2b77a0e",
+        "wallet": "0xe6De8485fAc1Eb4779978Bc455ef09e0E8c107Bb",
+        "name": "Sabo ☆"
+    },
+    {
+        "CONTINENT": 65,
+        "kingdomId": "63c452b0d47ee378866e16b6",
+        "wallet": "0xe6De8485fAc1Eb4779978Bc455ef09e0E8c107Bb",
+        "name": "Portgas. D.Ace"
+    },
+    {
+        "CONTINENT": 65,
+        "kingdomId": "640c11cb5f789e641a0be51a",
+        "wallet": "0xe6De8485fAc1Eb4779978Bc455ef09e0E8c107Bb",
+        "name": ".Franky."
+    },
+    {
+        "CONTINENT": 65,
+        "kingdomId": "640fe583ce4261486c84044a",
+        "wallet": "0xe6De8485fAc1Eb4779978Bc455ef09e0E8c107Bb",
+        "name": ".Nami."
+    },
+    {
+        "CONTINENT": 65,
+        "kingdomId": "6174468124149f728f5cb48c",
+        "wallet": "0xe6De8485fAc1Eb4779978Bc455ef09e0E8c107Bb",
+        "name": "Monkey D.Luffy◐"
     }
 ];
-
